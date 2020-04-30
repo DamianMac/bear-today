1de0240a40666655695a356d4e6d3a3af44d4789
damianmac <damian@damianm.com>
2020-04-30 11:18:05 +1000

    init

---
+++ src/bear-today/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cmd = "-g bear://x-callback-url/create?title=30%20April%202020%20-%20Thursday&tags=today%2F2020%2F04&show_window=no&timestamp=yes&text=Hi%20there%0A%0A%23%23%20Today%0A%0A%0A";
+            System.Diagnostics.Process.Start("open", cmd);
+
+            Console.WriteLine("Done");
+        }
+    }
+}
