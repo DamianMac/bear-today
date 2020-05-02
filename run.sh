36069b62bf5b5e3e92058b3738b12dca46d52ba1
damianmac <damian@damianm.com>
2020-05-02 13:36:25 +1000

    builds single file exe

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 pushd src/BearToday
-dotnet run
+dotnet clean
+dotnet publish -r osx-x64 /p:PublishSingleFile=true
 popd
+./src/BearToday/bin/Debug/netcoreapp3.1/osx-x64/publish/BearToday
