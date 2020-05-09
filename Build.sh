1fc5cae1aa23fdc16eb40bd7cca87d09592ef98f
damianmac <damian@damianm.com>
2020-05-09 16:54:16 +1000

    copy to home

--- Build.sh
+++ Build.sh
@@ -4,4 +4,4 @@ pushd src/BearToday
 dotnet clean
 dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true
 popd
-#cp ./src/BearToday/bin/Debug/netcoreapp3.1/osx-x64/publish/BearToday ./
+cp ./src/BearToday/bin/Debug/netcoreapp3.1/osx-x64/publish/BearToday ~/
