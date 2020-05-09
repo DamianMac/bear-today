2839d0dceda530d6a91e16d23eb7bf9c5f429787
damianmac <damian@damianm.com>
2020-05-09 17:01:11 +1000

    copied wrong binary

--- Build.sh
+++ Build.sh
@@ -2,6 +2,7 @@
 
 pushd src/BearToday
 dotnet clean
+dotnet build -c Release
 dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true
 popd
-cp ./src/BearToday/bin/Debug/netcoreapp3.1/osx-x64/publish/BearToday ~/
+cp ./src/BearToday/bin/Release/netcoreapp3.1/osx-x64/publish/BearToday ~/
